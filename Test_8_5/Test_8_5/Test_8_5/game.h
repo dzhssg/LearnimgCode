649a4d19959c030b934a3d4ce9a55966132d00e4
dzhssg <626397018@qq.com>
2023-08-08 18:25:06 +0800

    扫雷代码

---
+++ Test_8_5/Test_8_5/Test_8_5/game.h
@@ -0,0 +1,21 @@
+#pragma once
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+#define ROW 9
+#define COL 9
+#define ROWS ROW+2
+#define COLS COL+2
+#define EASY_COUNT 10;
+//函数的声明
+
+//初始化棋盘
+void InitBoard(char board[ROWS][COLS], int rows, int cols);
+
+//棋盘打印
+void DisplayBoard(char board[ROW][COL], int row, int col);
+
+//布置雷
+void SetMine(char mine[ROWS][COLS], int row, int col);
+//排查雷
+void FineMine(char mine[ROWS][COLS], char show[ROWS][COLS],int row, int col);
