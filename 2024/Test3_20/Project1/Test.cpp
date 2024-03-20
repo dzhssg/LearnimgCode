ac4e42d31ef7bdff38d1b7b5964e1b9d8c77de67
dzhssg <dzhssg6@outlook.com>
2024-03-20 22:47:19 +0800

    Learing namespaces in C++

---
+++ 2024/Test3_20/Project1/Test.cpp
@@ -0,0 +1,38 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+//#include<stdio.h>
+//
+//using namespace one;
+//
+//namespace one
+//{
+//  int x = 1;
+//
+//  struct Node
+//  {
+//    struct Node* next;
+//    int val;
+//  };
+//}
+// 
+//
+//int main()
+//{
+//  struct Node phead;
+//
+//  return 0;
+//}
+
+
+
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+
+  cout << "Hello Word" << endl;
+
+  return 0;
+}
